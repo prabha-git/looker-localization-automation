15ae8442ce780c04ac0266e5a26609cb1099d610
prabha-git <prabhakaran.mails@gmail.com>
2023-07-17 01:00:12 -0500

    Looker Localization Automation code Final version

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# looker-localization-automation
+# LookML Localization Automation
+
+This open-source code simplifies the localization of your LookML model files. The script parses through LookML files, extracts all user-facing strings (such as labels, descriptions, group_label, and group_item_label), and translates them using the Google Cloud Translate API. The translated strings are stored in a separate JSON file for each target language.
+
+## Repository Structure
+
+```plaintext
+.
+├── LICENSE
+├── README.md
+├── looker_google_translate_lang_mapping.json
+├── lookml_localization.py
+└── requirements.txt
+```
+
+## How to Use
+
+Follow the steps below to use this tool:
+
+1. Clone this repository to your local machine.
+2. Clone the Looker Project to your local machine.
+3. Set the 'GOOGLE_APPLICATION_CREDENTIALS' environment variable to point to your Google Cloud Service account key file. The service account should have the 'Cloud Translation API User' permission.
+
+Use the following command to clone this repository:
+
+```bash
+git clone https://github.com/prabha-git/looker-localization-automation.git
+```
+
+Use the following command to clone your Looker Project:
+
+```bash 
+git clone <<your looker git repo url>>
+```
+
+To run the localization automation script, you must have Python installed on your machine along with the packages listed in the `requirements.txt` file. Install the requirements using the following command:
+
+```bash
+pip install -r looker-localization-automation/requirements.txt
+```
+
+Once the requirements are installed, execute the script from the command line. The directory path should be the first argument, followed by language codes. For example, to translate to French (fr_FR) and Spanish (es_ES), run:
+
+```bash
+python looker-localization-automation/lookml_localization.py /your_path_to_looker_project fr_FR es_ES
+```
+
+The script leverages the `looker_google_translate_lang_mapping.json` file to map language codes to their Google Translate counterparts. To add another language, simply update this file, adhering to the existing format.
+
+Upon completion, the script will create localization files in the LookML root folder, denoted by the extension '.strings.json'.
+
+Be sure to include the following snippet in the manifest.lkml file to incorporate the localization files into the project:
+
+```bash
+localization_settings: {
+  default_locale: en
+  localization_level: permissive
+}
+```
+
+Finally, commit the localization files to your Looker Project and push to the remote repository.
+
+## Supported Languages
+
+The `looker_google_translate_lang_mapping.json` file currently includes the following languages:
+
+| Language              | Locale Code and Strings Filename |
+|-----------------------|----------------------------------|
+| English               | en                               |
+| Czech                 | cs_CZ                            |
+| German                | de_DE                            |
+| Spanish               | es_ES                            |
+| French                | fr_FR                            |
+| Hindi                 | hi_IN                            |
+| Italian               | it_IT                            |
+| Japanese              | ja_JP                            |
+| Korean                | ko_KR                            |
+| Lithuanian            | lt_LT                            |
+| Norwegian (Bokmål)    | nb_NO                            |
+| Dutch                 | nl_NL                            |
+| Polish                | pl_PL                            |
+| Brazilian Portuguese  | pt_BR                            |
+| Portuguese            | pt_PT                            |
+| Russian               | ru_RU                            |
+| Swedish               | sv_SE                            |
+| Thai                  | th_TH                            |
+| Turkish               | tr_TR                            |
+| Ukrainian             | uk_UA                            |
+| Simplified Chinese    | zh_CN                            |
+| Traditional Chinese   | zh_TW                            |
+
+Please note that for languages with variants like Chinese (with zh_CN and zh_TW for Simplified and Traditional Chinese respectively), Google Translate only supports a single code (zh). Therefore, the same translation will be used for both variants.
+
+## License
+
+This project is licensed under the terms specified in the LICENSE file.
--- lookml_localization.py
+++ lookml_localization.py
@@ -1,34 +1,55 @@
 import os
 import json
 import re
+import sys
 from google.cloud import translate_v2 as translate
-from google.oauth2.service_account import Credentials
 from html import unescape
-
-# Set the path of the Service Account key
-key_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-
-# Load the JSON key into a dictionary
-with open(key_path) as key_file:
-    key_dict = json.load(key_file)
 
 # Define patterns for extracting fields and detecting hidden elements
 field_pattern = re.compile(r'(dimension|measure|filter|parameter):\s*\w+\s*{([^}]*)}', re.DOTALL)
 hidden_pattern = re.compile(r'hidden:\s*yes', re.IGNORECASE)
 label_pattern = re.compile(r'\blabel:\s*"(.+?)"', re.DOTALL)
 description_pattern = re.compile(r'\bdescription:\s*"(.+?)"', re.DOTALL)
+group_label_pattern = re.compile(r'\bgroup_label:\s*"(.+?)"', re.DOTALL)
+group_item_label_pattern = re.compile(r'\bgroup_item_label:\s*"(.+?)"', re.DOTALL)
 
 # Prepare an empty dictionary to store results
 output = {}
 
-# Define target languages
-languages = ['en', 'es']
+# Load language mapping
+with open('./looker-localization-automation/looker_google_translate_lang_mapping.json') as f:
+    language_mapping = json.load(f)
+
+if len(sys.argv) < 3:
+    print("Error: You must provide the path to the LookML directory and at least one language code as arguments.")
+    with open('looker_google_translate_lang_mapping.json', 'r') as f:
+        lang_mapping = json.load(f)
+    print("Available options are:")
+    for code, details in lang_mapping.items():
+        print(f"{code} ( {details['name']})")
+    sys.exit()
+
+# Define target languages and LookML directory from the user input. The first argument (index 0) is the script name itself.
+lookml_root_folder = sys.argv[1]
+user_languages = sys.argv[2:]
+
+# Always add English to the user languages
+if 'en' not in user_languages:
+    user_languages.append('en')
+
+# Validate user input
+for lang in user_languages:
+    if lang not in language_mapping:
+        print(f"Language code {lang} is not supported. Please choose from the following options:")
+        for code, details in language_mapping.items():
+            print(f"{code} ( {details['name']})")
+        exit(1)
 
 # Create the Translate client using the key dictionary
-translate_client = translate.Client(credentials=key_dict)
+translate_client = translate.Client()
 
-# Walk through each file in the current directory and its subdirectories recursively
-for root, dirs, files in os.walk("."):
+# Walk through each file in the specified directory and its subdirectories recursively
+for root, dirs, files in os.walk(lookml_root_folder):
     for file in files:
         # Only consider .view.lkml files
         if file.endswith(".view.lkml"):
@@ -40,26 +61,35 @@ for root, dirs, files in os.walk("."):
                     if not hidden_pattern.search(field_content):
                         label_match = label_pattern.search(field_content)
                         description_match = description_pattern.search(field_content)
+                        group_label_match = group_label_pattern.search(field_content)
+                        group_item_label_match = group_item_label_pattern.search(field_content)
                         if label_match:
                             label = label_match.group(1)
                             output[label] = label
                         if description_match:
                             description = description_match.group(1)
                             output[description] = description
+                        if group_label_match:
+                            group_label = group_label_match.group(1)
+                            output[group_label] = group_label
+                        if group_item_label_match:
+                            group_item_label = group_item_label_match.group(1)
+                            output[group_item_label] = group_item_label
 
 # Translate and save results for each language
-for language in languages:
+for user_language in user_languages:
+    google_language = language_mapping[user_language]['google_translate_code']
     output_translated = {}
     for key, value in output.items():
-        if language == 'en':
+        if google_language == 'en':
             output_translated[key] = value
         else:
             # Translate text using Google Cloud Translate API
-            translation = translate_client.translate(value, target_language=language)['translatedText']
+            translation = translate_client.translate(value, target_language=google_language)['translatedText']
             # Unescape HTML characters
             translation = unescape(translation)
             output_translated[key] = translation
 
-    # Save results to '{language}.strings.json' in the root directory
-    with open(f'{language}_strings.json', 'w', encoding='utf-8') as f:
+    # Save results to '{language}.string.json' in the specified directory
+    with open(os.path.join(lookml_root_folder, f'{user_language}.string.json'), 'w', encoding='utf-8') as f:
         json.dump(output_translated, f, ensure_ascii=False, indent=2)
